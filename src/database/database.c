937178b179ada1bec1e3d11a035386c82de7553c
msterhuj <gabin.lanore@gmail.com>
2022-02-15 22:19:41 +0100

    Feat: add sql file loader

--- src/database/database.c
+++ src/database/database.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include "sqlite3.h"
 
-// open sqlite3 database and return a pointer to it
+/**
+ * @brief Create db file is not exist and open db file
+ * @param db_name
+ * @return sqlite3*
+ */
 sqlite3 *open_database(char *db_name) {
     sqlite3 *db;
     int rc;
@@ -14,4 +18,61 @@ sqlite3 *open_database(char *db_name) {
     }
 
     return db;
+}
+
+/**
+ * @brief delete db file
+ * @param db_name
+ */
+void delete_database(char *db_name) {
+    remove(db_name);
+}
+
+/**
+ * @brief exec sql script on db file
+ * @param db
+ * @param sql_file
+ * @return void
+ */
+int exec_sql_file(sqlite3 *db, char *sql_file) {
+    char *zErrMsg = 0;
+    int rc;
+    FILE *fp;
+    char *sql;
+    size_t size;
+
+    fp = fopen(sql_file, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Can't open file: %s\n", sql_file);
+        return -1;
+    }
+
+    fseek(fp, 0, SEEK_END);
+    size = ftell(fp);
+    rewind(fp);
+
+    sql = malloc(size + 1);
+    if (sql == NULL) {
+        fprintf(stderr, "Can't allocate memory\n");
+        return -1;
+    }
+
+    if (fread(sql, 1, size, fp) != size) {
+        fprintf(stderr, "Can't read file: %s\n", sql_file);
+        return -1;
+    }
+
+    sql[size] = '\0';
+
+    rc = sqlite3_exec(db, sql, NULL, 0, &zErrMsg);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+        return -1;
+    }
+
+    fclose(fp);
+    free(sql);
+
+    return 0;
 }
