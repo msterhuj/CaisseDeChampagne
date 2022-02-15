c21a92a0132997f7658139c3a8ee435718eedd1b
msterhuj <gabin.lanore@gmail.com>
2022-02-15 17:10:06 +0100

    Feat: add sdl2 and first screen

---
+++ deps.sh
@@ -0,0 +1,5 @@
+pacman -Syy
+pacman -Suu
+# lib for build app
+pacman -S mingw-w64-x86_64-toolchain base-devel
+pacman -S mingw-w64-x86_64-SDL2 mingw-w64-x86_64-SDL2_image mingw-w64-x86_64-SDL2_gfx mingw-w64-x86_64-SDL2_ttf
